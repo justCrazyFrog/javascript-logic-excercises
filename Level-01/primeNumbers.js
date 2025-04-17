7a2ee43b8f3beeb51017e83b7c623af90356b740
Administrator <administrator@Andreans-MacBook-Air.local>
2025-04-17 10:23:12 +0700

    Add questions to level 1 & 2

---
+++ Level-01/primeNumbers.js
@@ -0,0 +1,31 @@
+/*
+Buatlah sebuah program untuk mencari angka prima!
+
+Diberikan sebuah fungsi bernama primeNumbers yang menerima satu parameter bernama n.
+Carilah semua angka prima yang muncul dengan n sebagai batas (n juga termasuk).
+
+Jika tidak ditemukan angka prima outputnya adalah -1
+Jika ditemukan, meski hanya satu angka, outputnya adalah sebuah array yang diisi oleh semua angka prima yang ditemukan.
+
+n = 1 outputnya -1
+n = -4 outputnya -1
+n = 2 outputnya [2]
+n = 10 outputnya [2,3,5,7]
+
+*/
+
+function primeNumbers(n) {
+    
+};
+
+console.log(primeNumbers(10));
+// [ 2, 3, 5, 7 ]
+console.log(primeNumbers(1));
+// -1
+console.log(primeNumbers(30));
+// [
+//     2,  3,  5,  7, 11,
+//     13, 17, 19, 23, 29
+// ]
+console.log(primeNumbers(-4));
+// -1
---
+++ Level-02/bubbleSort.js
@@ -0,0 +1,21 @@
+/* 
+Bubble Sort
+===========
+
+Buatlah sebuah program untuk menyortir angka-angka secara menaik yang ada di tiap argumen dari pemanggilan function Bubble Sort.
+
+Bubble Sort adalah sebuah algoritma sortir sederhana yang membandingkan antara dua angka bersampingan.
+
+Gunakan bubble sort untuk menyortirnya.
+
+*/
+
+function bubbleSort(numbers) {
+
+};
+
+
+console.log(bubbleSort([5,4,3,2,1]));
+// [ 1, 2, 3, 4, 5 ]
+console.log(bubbleSort([4,-1,9,2,3]));
+// [-1, 2, 3, 4, 9]
