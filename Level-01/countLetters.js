c2cfd7d6c922f10328c4113b383d956c1af95ddf
Administrator <administrator@Andreans-MacBook-Air.local>
2025-04-22 17:06:55 +0700

    Add countLetters exercise on level 1

---
+++ Level-01/countLetters.js
@@ -0,0 +1,45 @@
+/*
+Count All Letters
+
+Diberikan sebuah function countLetters yang menerima sebuah parameter dengan nilai berbentuk string.
+
+Tugas dari fungsi ini adalah untuk menghitung berapa banyak huruf-huruf yang muncul pada string yang diterima.
+
+Contoh:
+function countLetters(word) {
+
+}
+
+console.log(countLetters("makan")); 
+{
+    m: 1,
+    a: 2,
+    k: 1,
+    n: 1
+}
+*/
+
+
+function countLetters(word) {
+    
+}
+
+console.log(countLetters("makan")); 
+// {
+//     m: 1,
+//     a: 2,
+//     k: 1,
+//     n: 1
+// }
+
+console.log(countLetters("XY-0778-12"));
+// { 
+//     '0': 1,
+//     '1': 1,
+//     '2': 1,
+//     '7': 2,
+//     '8': 1,
+//     X: 1,
+//     Y: 1,
+//     '-': 2 
+// }
